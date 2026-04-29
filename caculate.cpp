83192589b733ad563fd85d239177eef239f6ff8b
s1140521-wq <s1140521@dmhs.kh.edu.tw>
2026-04-29 15:15:50 +0800

    Create  caculate.cpp

---
+++ caculate.cpp
@@ -0,0 +1,19 @@
+
+int main() {
+    int  X1 =5+3;
+    int  X2 =5-3;
+    int  X1 =5*3;
+    int  X3 =5/3;
+    int  X4=5/3.0;
+   float X5=5/3.0;
+    int  X6 =5%3;
+    // 將 x1 到 x6 逐一輸出
+    cout << "x1 (5+3)  = " << x1 << endl;
+    cout << "x2 (5-3)  = " << x2 << endl;
+    cout << "x3 (5*3)  = " << x3 << endl;
+    cout << "x4 (5/3)  = " << x4 << endl;
+    cout << "x5 (5/3.0)= " << x5 << endl;
+    cout << "x6 (5%3)  = " << x6 << endl;
+
+    return 0;
+}
